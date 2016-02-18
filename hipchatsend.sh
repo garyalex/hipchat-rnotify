6465612ee9b8e57b2ec5b89ffe4b03df9d60ccfe
Gary Alexander <garyalex@gmail.com>
2016-02-18 16:55:52 +0200

    Fix

--- hipchatsend.sh
+++ hipchatsend.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ ! -f /usr/local/etc/hctoken ]; then
+    echo "No token!"
+    exit 1
+else
+    TOKEN=`cat /usr/local/etc/hctoken`
+fi
+
 if (( $# > 0 )); then
     MESSAGE=$1
     while [ "$2" != "" ]; do
         shift
         MESSAGE="$MESSAGE $1"
     done
-    /usr/local/hc/hipchat.pl -room "Support" -token "xJdr9kxIGsMkPsEhninWMvXB8YwdnwjcaR2wgCH3" -from "garyalex" -message $1
+    /usr/local/hc/hipchat.pl -room "Support" -token $TOKEN -from "garyalex" -message "$MESSAGE"
 else
     echo "No message!"
     exit 1
